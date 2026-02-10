c7ca509675a15faf50f37b269c8b0a27ef127b5e
Spirou1 <enzospironellipinheiro@gmail.com>
2026-02-10 00:54:57 -0300

    actually drawing stuff now

--- squares.c
+++ squares.c
@@ -7,6 +7,9 @@
 
 #include "dir_list.h"
 
+#define BLOCKS_NUM 30
+WINDOW *blocks[BLOCKS_NUM];
+
 int draw_screen(DirectoryList *dirlist, unsigned long long total) {
 
   initscr();
@@ -15,10 +18,12 @@ int draw_screen(DirectoryList *dirlist, unsigned long long total) {
   curs_set(0);
   start_color();
 
-  int rows, cols;
-  getmaxyx(stdscr, rows, cols);
+  int max_y, max_x;
+  getmaxyx(stdscr, max_y, max_x);
 
   init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_WHITE, COLOR_BLUE);
+
   wbkgd(stdscr, COLOR_PAIR(1));
 
   mvprintw(1, 2, "TOTAL DISK SIZE USED: %.2f GB",
@@ -26,21 +31,36 @@ int draw_screen(DirectoryList *dirlist, unsigned long long total) {
 
   refresh();
 
-  int height = 10;
-  int width = 20;
   int start_x = 10;
   int start_y = 5;
+  int height = 10;
 
-  WINDOW *block1 = newwin(height, width, start_y, start_x);
+  for (int i = 0; i < BLOCKS_NUM; i++) {
+    double ratio = (double)dirlist->data[i].bytes / total * 2;
+    int width = (int)(ratio * max_y);
 
-  init_pair(2, COLOR_WHITE, COLOR_BLUE);
-  wbkgd(block1, COLOR_PAIR(2));
+    if (start_x + width > max_x) {
+      start_y += height;
+      start_x = 10;
+    }
 
-  box(block1, 0, 0);
-
-  wrefresh(block1);
+    if (width < 10) {
+      width = 20;
+    }
+    blocks[i] = newwin(height, width, start_y, start_x);
+    wbkgd(blocks[i], COLOR_PAIR(2));
+    box(blocks[i], 0, 0);
+    mvwprintw(blocks[i], 1, 1, "NAME: %s", dirlist->data[i].path);
+    wrefresh(blocks[i]);
+    start_x += width + 1;
+  }
 
   getch();
+
+  for (int i = 0; i < BLOCKS_NUM; i++) {
+    delwin(blocks[i]);
+  }
+
   endwin();
 
   return 0;
